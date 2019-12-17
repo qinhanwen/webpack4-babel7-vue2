af34d16cb25d2de96eacab8b5218483d6e535163
qinhanwen <285074175@qq.com>
2019-12-17 14:15:41 +0800

    feat: 新增devServer.proxy配置

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    //devserver
-    PORT: 3000,
-    HOST: 'localhost',
-    AUTO_OPEN_BROWER: true,
-    HOT: true,
-    PROCESS: true,
-    INLINE:true,
-    HISTORY_API_FALLBACK:true,
-    CONTENT_BASE:'./static',
+  //devserver
+  PORT: 3000,
+  HOST: 'localhost',
+  AUTO_OPEN_BROWER: true,
+  HOT: true,
+  PROCESS: true,
+  INLINE: true,
+  HISTORY_API_FALLBACK: true,
+  CONTENT_BASE: './static',
 
-    //watch
-    WATCH: true,
+  //watch
+  WATCH: true,
 
-    //watchoptions
-    IGNORED:'/node_modules/',
-    AGGREGATE_TIMEOUT:300
+  //watchoptions
+  IGNORED: '/node_modules/',
+  AGGREGATE_TIMEOUT: 300
 }
